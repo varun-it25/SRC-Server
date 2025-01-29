c93e7d4295bbbfb9943229ca0f2d3d5049932cdf
varun kangotra <varunkangotra.it25@gmail.com>
2025-01-30 00:34:29 +0530

    event by id error corrected

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/event/:event_id', async (req, res) => {
   try {
     const eventData = await Event.findById(event_id);
     if (!eventData) return res.status(404).json({ message: 'Event not found.' });
-    res.status(200).json({...eventData, status: eventData.event_date});
+    res.status(200).json(eventData);
   } catch (err) {
     res.status(500).json({ message: 'Error retrieving event', error: err.message });
   }
@@ -162,6 +162,15 @@ app.delete('/gallery/delete_file/:id', async (req, res) => {
   try {
     await Gallery.findOneAndDelete({_id: id})
     res.status(201).send(`File deleted.`)
+  } catch (err) {
+    res.status(500).json({ message: 'Error delete file', error: err.message });
+  }
+});
+
+app.delete('/gallery/delete_all_files', async (req, res) => {
+  try {
+    await Gallery.deleteMany({})
+    res.status(201).send(`All file deleted.`)
   } catch (err) {
     res.status(500).json({ message: 'Error delete file', error: err.message });
   }
