6c632120f6403ef5c91ec8208ccd742a1b6bd078
varun kangotra <varunkangotra.it25@gmail.com>
2025-01-29 23:54:22 +0530

    Error Correted on Updated Gallery Upload

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ const eventSchema = new mongoose.Schema({
 const galleySchema = new mongoose.Schema({
   file_type: String,
   file_name: String,
-  file_size: number,
-  file_url: number,
+  file_size: Number,
+  file_url: String,
 }, { versionKey: false });
 
 const memberSchema = new mongoose.Schema({
