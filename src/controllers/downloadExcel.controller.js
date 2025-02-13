96a54d2890534dfd000745cd7504068a68308fb3
Varun kangotra <varunkangotra.it25@gmail.com>
2025-02-13 17:40:52 +0530

    Update download Excel Error Fixed

--- src/controllers/downloadExcel.controller.js
+++ src/controllers/downloadExcel.controller.js
@@ -1,7 +1,7 @@
 export const downloadFeedbacksExcel = async(req, res) => {
-    res.download(`../../store/feedbacksData.xlsx`)
+    res.download(`./store/feedbacksData.xlsx`)
 }
 
 export const downloadRegistersExcel = async(req, res) => {
-    res.download(`../../store/registersData.xlsx`)
+    res.download(`./store/registersData.xlsx`)
 }
