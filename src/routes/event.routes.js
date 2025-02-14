a290ed4c9ef699fdd835b82bc0ad591a3e515fbd
Avi Maheshwari <avimaheshwari04@gmail.com>
2025-02-14 16:52:41 +0530

    Recent feedbacks added || Upcoming Events added

--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -4,10 +4,10 @@ import {
   createEvent,
   getEventById,
   getAllEvents,
-  getUpcomingEvents,
   deleteEvent,
   deleteAllEvents,
   updateEvent,
+  getMostRecentUpcomingEvent,
 } from '../controllers/event.controller.js';
 
 const router = express.Router();
@@ -15,9 +15,9 @@ const router = express.Router();
 router.post('/create-event', createEvent);
 router.get('/event/:event_id', getEventById);
 router.get('/events', getAllEvents);
-router.get('/events/upcoming', getUpcomingEvents);
 router.delete('/event/delete/:id', deleteEvent);
 router.delete('/event/delete_all', deleteAllEvents);
 router.patch('/events/update/:event_id', updateEvent);
+router.get('/events/upcoming', getMostRecentUpcomingEvent);
 
 export default router;
--- src/routes/feedback.routes.js
+++ src/routes/feedback.routes.js
@@ -1,6 +1,6 @@
 // routes/feedbackRoutes.js
 import express from 'express';
-import { getAllFeedbacks, addFeedback, getFeedbacksByEvent, getFeedbackById } from '../controllers/feedback.controller.js';
+import { getAllFeedbacks, addFeedback, getFeedbacksByEvent, getFeedbackById, getFeedbacksForMostRecentEvent } from '../controllers/feedback.controller.js';
 
 const router = express.Router();
 
@@ -8,5 +8,5 @@ router.get('/feedbacks/all', getAllFeedbacks);
 router.post('/add-feedback', addFeedback);
 router.get('/feedbacks/event/:event_id', getFeedbacksByEvent);
 router.get('/feedback/:feedback_id', getFeedbackById);
-
+router.get('/feedbacks/most-recent', getFeedbacksForMostRecentEvent);
 export default router;
