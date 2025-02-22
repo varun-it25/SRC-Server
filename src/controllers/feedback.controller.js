fcaf84fb070e7cf8763de2393271f9edd1f185dd
Avi Maheshwari <avimaheshwari04@gmail.com>
2025-02-22 15:33:56 +0530

    fixed feedback sorting problem

--- src/controllers/feedback.controller.js
+++ src/controllers/feedback.controller.js
@@ -1,13 +1,15 @@
 // controllers/feedbackController.js
-import {Feedback} from '../models/feedback.models.js';
-import { Event } from '../models/event.models.js';
+import { Feedback } from "../models/feedback.models.js";
+import { Event } from "../models/event.models.js";
 
 export const getAllFeedbacks = async (req, res) => {
   try {
     const feedbackData = await Feedback.find({});
     res.status(200).json(feedbackData);
   } catch (err) {
-    res.status(500).json({ message: 'Error retrieving feedbacks', error: err.message });
+    res
+      .status(500)
+      .json({ message: "Error retrieving feedbacks", error: err.message });
   }
 };
 
@@ -15,48 +17,66 @@ export const addFeedback = async (req, res) => {
   const { event_id, name, rtu_roll_no, mobile_no, experience } = req.body;
 
   try {
-    const feedbackData = new Feedback({ event_id, name, rtu_roll_no, mobile_no, experience });
+    const feedbackData = new Feedback({
+      event_id,
+      name,
+      rtu_roll_no,
+      mobile_no,
+      experience,
+    });
     await feedbackData.save();
-    res.status(200).send('Feedback added');
+    res.status(200).send("Feedback added");
   } catch (err) {
-    res.status(500).json({ message: 'Error adding feedback', error: err.message });
+    res
+      .status(500)
+      .json({ message: "Error adding feedback", error: err.message });
   }
 };
 
 export const getFeedbacksByEvent = async (req, res) => {
   const { event_id } = req.params;
-  if (!event_id) return res.status(400).json({ message: 'Event id is required.' });
+  if (!event_id)
+    return res.status(400).json({ message: "Event id is required." });
 
   try {
     const feedbacksData = await Feedback.find({ event_id });
     res.status(200).json(feedbacksData);
   } catch (err) {
-    res.status(500).json({ message: 'Error retrieving feedbacks', error: err.message });
+    res
+      .status(500)
+      .json({ message: "Error retrieving feedbacks", error: err.message });
   }
 };
 
 export const getFeedbackById = async (req, res) => {
   const { feedback_id } = req.params;
-  if (!feedback_id) return res.status(400).json({ message: 'Feedback id is required.' });
+  if (!feedback_id)
+    return res.status(400).json({ message: "Feedback id is required." });
 
   try {
     const feedbackData = await Feedback.findById(feedback_id);
     res.status(200).json(feedbackData);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Error retrieving feedback', error: err.message });
+    res
+      .status(500)
+      .json({ message: "Error retrieving feedback", error: err.message });
   }
 };
 
 export const getFeedbacksForMostRecentEvent = async (req, res) => {
-  try {  
+  try {
     const currentDate = new Date();
-    const mostRecentEvent = await Event.findOne({ event_date: { $lte: currentDate } }).sort({ event_date: 1 });      
+    const mostRecentEvent = await Event.findOne({
+      event_date: { $lte: currentDate },
+    }).sort({ event_date: -1 });
     const feedbackData = await Feedback.find({ event_id: mostRecentEvent._id });
-   
+
     res.status(200).json(feedbackData);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Error retrieving feedbacks', error: err.message });
+    res
+      .status(500)
+      .json({ message: "Error retrieving feedbacks", error: err.message });
   }
 };
